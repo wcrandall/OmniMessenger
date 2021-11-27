7da49dfacdef8ad47a78a9c6003f209ad96730d6
wcrandall <aweserific@gmail.com>
2021-11-27 14:44:41 -0500

    added space

--- OmniMessengerDesktop/ViewModels/MainWindowViewModel.cs
+++ OmniMessengerDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using OmniMessengerDesktop.Models;
 using ReactiveUI;
@@ -12,7 +10,9 @@ namespace OmniMessengerDesktop.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private readonly HttpClient client;
+
         private string greeting= "hi";
+
         public string Greeting { 
             get
             {
@@ -24,27 +24,30 @@ namespace OmniMessengerDesktop.ViewModels
                 this.RaiseAndSetIfChanged(ref greeting, value);
             } 
         }
+
         public MainWindowViewModel()
         {
             HttpClientHandler clientHandler = new HttpClientHandler();
             clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => {return true;};
             this.client = new HttpClient(clientHandler); 
         }
+
         public async void GetTheStringCommand(){
             Uri uri = new Uri(string.Format("https://localhost:5001/message", string.Empty));
             HttpResponseMessage response = await client.GetAsync(uri);
             List<ContactInfo> contactInfos = null;
+
             if(response.IsSuccessStatusCode){
                 string content = await response.Content.ReadAsStringAsync();
-                var options = new JsonSerializerSettings(
 
-                );
                 if(content != null)
                 {
                     contactInfos = JsonConvert.DeserializeObject<List<ContactInfo>>(content);
                 }
+
                 Console.WriteLine(content);
             }
+
             Console.WriteLine(response.IsSuccessStatusCode.ToString());
 
             if(contactInfos == null || contactInfos.Count == 0){
@@ -52,11 +55,12 @@ namespace OmniMessengerDesktop.ViewModels
             }
             
             this.Greeting = contactInfos[0].FullName + " has an email address " + contactInfos[0].Email;
+            
             Console.WriteLine(this.Greeting);
         }
 
         public void DoSomething(){
-            this.Greeting = "fuck you";
+            this.Greeting = "Do your homework";
         }
         
     }
